11b7f65d6213f65c6c74986cee4ff4d1567ce61d
crynobone <crynobone@gmail.com>
2012-11-24 02:50:54 +0800

    Add more example, for pane.orchestra
    Signed-off-by: crynobone <crynobone@gmail.com>

--- views/pane/orchestra.blade.php
+++ views/pane/orchestra.blade.php
@@ -5,7 +5,16 @@
 Event::listen('orchestra.started: backend', function ()
 {
 	$p = Orchestra\Widget::make('pane.orchestra');
+
 	$p->add('demo')->title('Demo for `pane.orchestra`')
+		->attr(array('class' => 'span12'))
 		->content(View::make('preview::pane.orchestra'));
 
+	$p->add('small')->title('Small widget')
+		->attr(array('class' => 'span3'))
+		->content('Use span3 class');
+
+	$p->add('large')->title('Large widget')
+		->attr(array('class' => 'span8'))
+		->content('Use span8 class');
 });</pre>
